723dbc4fa9389e62ef4dedc2baa17dd100f3069d
boxgaming <75969133+boxgaming@users.noreply.github.com>
2025-12-14 16:06:36 -0600

    Initial multiplayer support

---
+++ gxs-client-v0.bas
@@ -0,0 +1,202 @@
+Option Explicit
+
+Const MSG_HOST_GAME = 1
+Const MSG_JOIN_GAME = 2
+Const MSG_HOST_DISCONNECTED = 3
+Const MSG_CLIENT_DISCONNECTED = 4
+Const MSG_ERROR = 5
+Const MSG_CLOSE = 6
+'Const GXS_WS_URL = "ws://localhost:8080/"
+'Const GXS_URL = "http://localhost:8080/v0"
+Const GXS_WS_URL = "wss://gxapi.boxgaming.co/"
+Const GXS_URL = "https://gxapi.boxgaming.co/v0"
+Const LB_URL = GXS_URL + "/lb"
+
+' library exports
+Export MSG_HOST_GAME As HOST_GAME, MSG_JOIN_GAME As JOIN_GAME
+Export MSG_CLIENT_DISCONNECTED As CLIENT_DISCONNECTED, MSG_HOST_DISCONNECTED As HOST_DISCONNECTED
+Export MSG_ERROR As ERROR, MSG_CLOSE As CLOSE
+Export HostGame, JoinGame, SendMessage, SendHostMessage, RegisterEvent, IsHost, SessionId, ClientId
+Export LBGet As LeaderboardGet, LBAdd As LeaderboardUpdate, LBCreate As LeaderboardCreate, LBRestrict As LeaderboardRestrict
+
+Dim Shared websocket As Object
+Dim Shared As String sid, cid
+Dim Shared As Integer mode
+Dim Shared eventMap()
+
+Sub HostGame
+    Connect MSG_HOST_GAME
+End Sub
+
+Sub JoinGame (ssid As String, pname As String)
+    sid = ssid
+    Connect MSG_JOIN_GAME
+End Sub
+
+Sub SendHostMessage (mtype As Integer, msg As Object)
+    SendMessage mtype, msg, "HOST"
+End Sub
+
+Sub SendMessage (mtype As Integer, msg As Object, toCid As String)
+    Dim m As Object
+    m.type = mtype
+    m.sid = sid
+    m.cid = cid
+    m.data = msg
+    m.to = toCid
+    Send m
+End Sub
+
+Sub OnOpen 
+    Dim msg As Object
+    msg.type = mode
+    msg.sid = sid
+    Send msg
+End Sub
+
+Sub OnClose (e As Object)
+    If eventMap(MSG_CLOSE) Then
+        Dim callback As Sub
+        callback = eventMap(MSG_ERROR)
+        callback e
+    End If
+End Sub
+
+Sub OnError (e As Object)
+    If eventMap(MSG_ERROR) Then
+        Dim callback As Sub
+        callback = eventMap(MSG_ERROR)
+        callback e
+    End If
+End Sub
+
+Sub OnMessage (msg As Object)
+    If msg.type = MSG_HOST_GAME Then
+        sid = msg.sid
+        cid = msg.cid
+    ElseIf msg.type = MSG_JOIN_GAME Then
+        If mode <> MSG_HOST_GAME Then cid = msg.cid
+    End If
+    If eventMap(msg.type) Then
+        Dim callback As Sub
+        callback = eventMap(msg.type)
+        callback msg
+    End If
+End Sub
+
+Sub Send (msg As Object)
+$If Javascript Then
+    websocket.send(JSON.stringify(msg));
+$End If
+End Sub
+
+Sub Connect (cmode As Integer)
+    mode = cmode
+    
+    Dim As Sub o, e, m, c
+    o = @OnOpen
+    e = @OnError
+    m = @OnMessage
+    c = @OnClose
+    
+$If Javascript Then
+    websocket = new WebSocket(GXS_WS_URL);
+    websocket.addEventListener("open", o);
+    websocket.addEventListener("close", c);
+    websocket.addEventListener("error", e);
+    websocket.addEventListener("message", function(e) {
+        m(JSON.parse(e.data)); 
+    });
+$End If
+End Sub
+
+Sub RegisterEvent (etype As Integer, callback As Sub)
+    eventMap(etype) = callback    
+End Sub
+
+Function IsHost
+    If mode = MSG_HOST_GAME Then
+        IsHost = -1
+    Else
+        IsHost = 0
+    End If
+End Function
+
+Function SessionId
+    SessionId = sid
+End Function
+
+Function ClientId
+    ClientId = cid
+End Function
+
+
+' -------------------------------------------------------------------------
+' Leaderboard methods
+' -------------------------------------------------------------------------
+
+Function LBCreate 
+    Dim res As Object
+    res = Fetch(LB_URL + "/register")
+    LBCreate = res.text
+End Function
+
+Function LBRestrict (did As String, gid As String, restrict As String)
+    Dim As Object r
+$If Javascript Then
+    r = await fetch(LB_URL + "/restrict", {
+        method: "GET",
+        headers: { 
+            "X-DID": did,
+            "X-GID": gid,
+            "X-Restrict-To": restrict
+        }});
+$End If
+    LBRestrict = r.status
+End Function
+
+
+Function LBGet (id As String, results() As Object)
+    Dim As Object r, scores, s
+    Dim As Integer i
+$If Javascript Then
+    r = await fetch(LB_URL, {
+        method: "GET",
+        headers: { 
+            "Content-Type": "application/json",
+            "X-GID": id
+        }});
+    if (r.status == 200) {
+        scores = await r.json();
+$End If
+        ReDim results(scores.length) As Object
+$If Javascript Then
+        for (i=0; i < scores.length; i++) {
+            s = scores[i];
+            var d = new Date(s.ts);
+            s.date = d.toLocaleDateString();
+            s.time = d.toLocaleTimeString();
+$End If
+            results(i+1) = s
+$If Javascript Then
+        }
+    }    
+$End If
+    LBGet = r.status
+End Sub
+
+Function LBAdd (id As String, score As Integer, pname As String)
+    Dim s As Object
+    s.score = score
+    s.name = pname
+$If Javascript Then
+    r = await fetch(LB_URL, {
+        method: "POST",
+        headers: { 
+            "Content-Type": "application/json",
+            "X-GID": id
+        },
+        body: JSON.stringify(s)});
+$End If
+    LBAdd = r.status
+End Function
--- server.js
+++ server.js
@@ -1,131 +1,376 @@
-const http = require('node:http');
+import { createRequire } from "module";
+import { WebSocketServer } from "ws";
+const require = createRequire(import.meta.url);
+const fs = require("node:fs");
+const http = require("node:http");
+const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const LB_FILE = "leaderboard.json";
+require("dotenv").config();
 
-const hostname = '127.0.0.1';
+const s3 = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+});
+
+const hostname = "127.0.0.1";
 const port = process.env.PORT || 8080;
 
-const data = {};
-const lb = {};
+let lb = {};
+let lbmap = {};
+let omap = {};
+const allowedOrigins = {};
+const DEVID = process.env.DEVID;
+allowedOrigins["https://qbjs.org"] = 1;
+allowedOrigins["https://boxgaming.github.io"] = 1;
+allowedOrigins["http://localhost:8080"] = 1;
 
 const server = http.createServer((req, res) => {
-  console.log("URL: " + req.url);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+    console.log("URL: " + req.url);
+    console.log("Method: " + req.method);
+    console.log("Headers:");
+    for (var h in req.headers) {
+        console.log("    -> " + h + ": " + req.headers[h]);
+    }
+    console.log("-------------------------------------------------------");
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
 
-  /*if (req.url.startsWith("/v0/scs/")) {
-    scs(req.url.substring(8), req, res);
-    //console.log(req.method + ":" + req.url);
-  }
-  else */
-  if (req.url.startsWith("/v0/lb")) {
-    leaderboard(req, res);
-  }
-  else {
-    res.end('Coming soon...\n');
-  }
 
-  function leaderboard(req, res) {
-    console.log("leaderboard: " + req.url);
-    console.log("method: " + req.method);
-    console.log("headers");
-    for (h in req.headers) {
-      console.log(" -> " + h + ": " + req.headers[h]);
-    }
-    let origin = req.headers["origin"];
-    if (origin == "http://localhost:8888" || origin == "https://qbjs.org") {
-      res.setHeader("Access-Control-Allow-Origin", origin);
-      res.setHeader("Access-Control-Allow-Headers", "Content-Type,X-GID");
-    }
-    else {
-      res.end("");
-      return;
-    }
-    if (req.url.endsWith("/register")) {
-      let id = crypto.randomUUID();
-      lb[id] = [];
-      res.end(id);
-    }
-    else if (req.method == "GET") {
-      console.log("GET section ------------------------");
-      let gid = req.headers["x-gid"];
-      console.log(gid);
-      let board = lb[gid];
-      if (board) {
-        board.sort((a, b) => b.score - a.score);
-        res.end(JSON.stringify(board));
-      }
-      else {
-        res.end("{}");
-      }
-    }
-    else if (req.method == "POST") {
-      console.log("POST section ------------------------");
-      let gid = req.headers["x-gid"];
-      console.log(gid);
-      let board = lb[gid];
-      console.log("board:" + board)
-      if (board) {
-        let body = [];
-        req.on("data", chunk => {
-          body += chunk;
-        })
-        .on('end', () => {
-          console.log("body:" + body);
-          try {
-            let s = JSON.parse(body);
-            board.push(s);
-            req.end("OK");
-          }
-          catch (e) {
-            console.log(e);
-            res.end("");
-          }
-          //body = Buffer.concat(body);
-        });
-      }
-      else {
-        res.end("");
-      }
-    }
-    else {
-      res.end("");
-    }
-  }
-
-  function scs(action, req, res) {
-    console.log(action);
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888");
-    if (action.startsWith("register")) {
-      let id = crypto.randomUUID();
-      data[id] = new Uint8Array(0);
-      res.end(id);
-    }
-    else {
-      let body = [];
-      req.on("data", chunk => {
-          body.push(chunk);
-        })
-        .on('end', () => {
-          console.log("body:" + body);
-          body = Buffer.concat(body);
-          if (req.method == "POST") {
-            data[action] = body;
-            res.end("OK");
-          }
-          else {
-            let result = data[action];
-            console.log(result);
-            if (result == undefined) {
-              result = "";
-            }
-            res.setHeader("Content-Type", "application/octet-stream");
-            res.setHeader("Content-Length", result.length);
-            res.end(result);
+    /*if (req.method == "OPTIONS") {
+        if (board && board.restrictOrigin) {
+            res.setHeader("Access-Control-Allow-Origin", board.restrictOrigin);
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type,X-GID,X-Restrict-To")
         }
-      });
+        res.end();
     }
-  }
+    else*/ if (req.url.startsWith("/v0/gxs.bas")) {
+        if (!checkOrigin(req, res)) { return; };
+
+        fs.readFile("gxs-client-v0.bas", function (error, content) {
+            if (error) {
+                // TODO: probably should do some error handling
+            }
+            else {
+                res.statusCode = 200;
+                res.end(content);
+            }
+        });
+    }
+    else if (req.url.startsWith("/v0/lb")) {
+        leaderboard(req, res);
+    }
+    else {
+        res.end('Coming soon...\n');
+    }
+
+    function checkOrigin(req, res) {
+        let origin = req.headers["origin"];
+        let gid = req.headers["x-gid"];
+        let board = lb[gid];
+        console.log(origin + ":" + gid + ":" + board);
+
+        if (req.method == "OPTIONS") {
+            if (allowedOrigins[origin]) {
+                res.setHeader("Access-Control-Allow-Origin", origin);
+                res.setHeader("Access-Control-Allow-Headers", "Content-Type,X-GID,X-Restrict-To,X-DID");
+            }
+            res.end();
+            return false;
+        }
+
+        if (board && board.restrictOrigin) {
+            console.log("GOT HERE! " + board.restrictOrigin);
+            if (origin == board.restrictOrigin) {
+                res.setHeader("Access-Control-Allow-Origin", origin);
+                res.setHeader("Access-Control-Allow-Headers", "Content-Type,X-GID");
+            }
+            else {
+                res.statusCode = 200;
+                res.end();
+                return false;
+            }
+        }
+        else if (origin == "https://boxgaming.github.io" || origin == "https://qbjs.org" || origin == "http://localhost:8080") {
+            res.setHeader("Access-Control-Allow-Origin", origin);
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type,X-GID,X-Restrict-To,X-DID");
+        }
+        else {
+            res.statusCode = 200;
+            res.end();
+            return false;
+        }
+
+        return true;
+    }
+
+    async function leaderboard(req, res) {
+        //console.log("leaderboard: " + req.url);
+        //console.log("method: " + req.method);
+        //console.log("headers");
+        //for (var h in req.headers) {
+        //    console.log(" -> " + h + ": " + req.headers[h]);
+        //}
+        if (!checkOrigin(req, res)) { console.log("check origin failed"); return; }
+
+        if (req.url.endsWith("/register")) {
+            //if (!checkOrigin(req, res)) { return; }
+
+            let id = crypto.randomUUID();
+            lb[id] = { scores: [] };
+            lbmap[id] = {};
+            res.end(id);
+            console.log("register: " + id);
+        }
+        else if (req.url.endsWith("/restrict")) {
+            //if (!checkOrigin(req, res)) { return; }
+            let did = req.headers["x-did"];
+            if (did != DEVID) {
+                res.end();
+                return;
+            }
+            let gid = req.headers["x-gid"];
+            let rorigin = req.headers["x-restrict-to"];
+            console.log("restrict: " + gid + " : " + rorigin);
+            let board = lb[gid];
+            if (board && rorigin) {
+                board.restrictOrigin = rorigin;
+                allowedOrigins[rorigin] = true;
+                s3Save(LB_FILE, JSON.stringify(lb));
+            }
+            res.end();
+        }
+        else if (req.method == "GET") {
+
+            //console.log("GET section ------------------------");
+            let gid = req.headers["x-gid"];
+            console.log("GET: " + gid);
+            let board = lb[gid];
+            if (board) {
+                board.scores.sort((a, b) => b.score - a.score);
+                res.end(JSON.stringify(board.scores));
+            }
+            else {
+                res.end("{}");
+            }
+        }
+        else if (req.method == "POST") {
+            //console.log("POST section ------------------------");
+            let gid = req.headers["x-gid"];
+            console.log("POST: " + gid);
+            let board = lb[gid];
+            //console.log("board:" + board)
+            if (board) {
+                let body = [];
+                req.on("data", chunk => {
+                    body += chunk;
+                })
+                    .on('end', () => {
+                        //console.log("body:" + body);
+                        try {
+                            let s = JSON.parse(body);
+                            var entry = lbmap[gid][s.name];
+                            if (entry) {
+                                if (s.score > entry.score) {
+                                    entry.score = s.score;
+                                    entry.ts = Date.now();
+                                }
+                            }
+                            else {
+                                s.ts = Date.now();
+                                board.scores.push(s);
+                                lbmap[gid][s.name] = s;//entry.scores(idx);
+                            }
+                            res.end("OK");
+                            //fs.writeFileSync("leaderboard.json", JSON.stringify(lb), "utf8");
+                            s3Save(LB_FILE, JSON.stringify(lb));
+                        }
+                        catch (e) {
+                            console.log(e);
+                            res.end("");
+                        }
+                    });
+            }
+            else {
+                res.end("");
+            }
+        }
+        else {
+            res.end("");
+        }
+    }
 });
 
+
+async function s3Save(filename, text) {
+    const params = {
+        Bucket: process.env.S3_BUCKET,
+        Key: process.env.S3_ROOT + filename,
+        Body: text,
+        ContentType: "text/plain"
+    };
+
+    await s3.send(new PutObjectCommand(params));
+}
+
+async function s3Load(filename) {
+    const params = {
+        Bucket: process.env.S3_BUCKET,
+        Key: process.env.S3_ROOT + filename
+    };
+
+    var response = await s3.send(new GetObjectCommand(params));
+    return await response.Body.transformToString();
+}
+
+async function init() {
+    //console.log("TEST: " + process.env.TEST)
+    //console.log("AK: " + process.env.AWS_ACCESS_KEY_ID);
+    /*    
+            // Prepare S3 upload
+            const params = {
+                Bucket: process.env.S3_BUCKET,
+                Key: process.env.S3_ROOT + "test2.txt",
+                Body: "This is text content.",
+                ContentType: "text/plain"
+            };
+    
+            await s3.send(new PutObjectCommand(params));
+        
+            var response = await s3.send(new GetObjectCommand(params));
+            console.log(await response.Body.transformToString());
+            */
+    //await s3Save(LB_FILE, "{}");
+    //console.log("---------> " + await s3Load(LB_FILE));
+    try {
+        //var lbtext = fs.readFileSync("leaderboard.json", "utf8");
+        var lbtext = await s3Load(LB_FILE);
+        lb = JSON.parse(lbtext);
+        for (var key in lb) {
+            //console.log("k:" + key);
+            lbmap[key] = {};
+            var board = lb[key];
+            var scores = [];
+            if (board.scores == undefined) {
+                lb[key] = { "scores": board };
+                //console.log(lb[key]);
+                scores = lb[key].scores;
+            }
+            else {
+                scores = board.scores;
+            }
+            //console.log("board.restrictOrigin: " + board.restrictOrigin);
+            if (board.restrictOrigin) {
+                allowedOrigins[board.restrictOrigin] = true;
+                console.log("Adding origin: " + board.restrictOrigin);
+            }
+            for (var i = 0; i < scores.length; i++) {
+                lbmap[key][scores[i].name] = scores[i];
+            }
+        }
+        console.log(JSON.stringify(lb));
+        console.log("--------------------------------------------------------------");
+        console.log(JSON.stringify(lbmap));
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+await init();
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+//var clients = [];
+const HOST = 1;
+const CLIENT = 2;
+const MSG_HOST_GAME = 1;
+const MSG_JOIN_GAME = 2;
+const MSG_HOST_DISCONNECTED = 3;
+const MSG_CLIENT_DISCONNECTED = 4;
+var sessions = {};
+
+const wss = new WebSocketServer({ server });
+wss.on("connection", function connection(ws) {
+
+  //clients.push(ws);
+  ws.on("message", function message(data) {
+    var msg = JSON.parse(data);
+    if (msg.type == MSG_HOST_GAME) {
+        var sid = crypto.randomUUID();
+        var cid = crypto.randomUUID();
+        ws.ctype = HOST;
+        ws.sid = sid;
+        ws.cid = cid;
+        sessions[sid] = { host: ws, clients: { } };
+        sessions[sid].clients[cid] = ws;
+        msg.sid = sid;
+        msg.cid = cid;
+        ws.send(JSON.stringify(msg));
+    }
+    else if (msg.type == MSG_JOIN_GAME) {
+        var session = sessions[msg.sid];
+        if (!session) {
+            console.log("Session not found: " + msg.sid);
+            return;
+        }
+        var cid = crypto.randomUUID();
+        ws.ctype = CLIENT;
+        ws.cid = cid;
+        ws.sid = msg.sid;
+        session.clients[cid] = ws;
+        msg.cid = cid;
+        ws.send(JSON.stringify(msg));
+        session.host.send(JSON.stringify(msg));
+        console.log(JSON.stringify(session.clients));
+    }
+    else {
+        var session = sessions[msg.sid];
+        if (!session) {
+            console.log("Session not found: " + msg.sid);
+            return;
+        }
+        if (msg.to == "HOST") {
+            session.host.send(JSON.stringify(msg));
+        }
+        else {
+            for (var cid in session.clients) {
+                //console.log(cid);
+                if (!msg.to || msg.to == "" || msg.to == cid) {
+                    session.clients[cid].send(JSON.stringify(msg));
+                }
+            }
+        }
+    }
+    //console.log(msg.type);
+  });
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    console.log(ws.sid);
+    var session = sessions[ws.sid];
+    //console.log(session);
+    if (session) {
+        var msg = {
+            type: MSG_CLIENT_DISCONNECTED,
+            sid: ws.sid,
+            cid: ws.cid
+        }
+        if (ws == session.host) { msg.type = MSG_HOST_DISCONNECTED }
+        delete session.clients[ws.cid];
+        var ccount = 0;
+        for (var cid in session.clients) {
+            console.log(JSON.stringify(msg));
+            session.clients[cid].send(JSON.stringify(msg));
+            ccount++;
+        }
+        if (ccount < 1) {
+            console.log("All clients disconnected, removing session");
+            delete session[ws.sid];
+        }
+    }
+  });
+});
+
